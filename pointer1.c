96716a4fd6b270bcc475d0ff0403fb47df084de8
huiyzhou <hzhou@ncsu.edu>
2026-08-26 14:56:58 -0400

    Correct pointer usage and address calculations

--- pointer1.c
+++ pointer1.c
@@ -17,3 +17,29 @@ int main()
 
     //free(p);
 }
+
+
+//Corrected version:
+#include <iostream>
+int main()
+{
+    int a;
+    int b;
+    void * x = static_cast<void *> (&a);
+    void * y = static_cast<void *> (&b);
+    std::cout << "Address difference between a and b:" << (long long)x - (long long)y << std::endl;
+    std::cout << static_cast<void*> (&a) << std::endl;
+    std::cout << static_cast<void*> (&b) << std::endl;
+
+    int *** addr0;
+    int ** addr1;
+    int * p;
+    p = (int *) malloc(100);
+    addr1 = &p;
+    addr0 = & addr1;
+    std::cout << "p value:" << p << " address of p: " << addr1 << std::endl;
+    std::cout << addr0;    
+
+    free(p);
+    return 0;
+}
